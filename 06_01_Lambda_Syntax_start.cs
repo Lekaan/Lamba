5bdb394a3937124e8f9f712967691af0aff9efb3
kaans <kaans@DESKTOP-U4CBDT5>
2017-02-22 08:34:33 +0100

    Add project files.

---
+++ 06_01_Lambda_Syntax_start.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Examples
+{
+    class Program
+    {
+        #region Class Definitions
+        public class Customer
+        {
+            public string First { get; set; }
+            public string Last { get; set; }
+            public string State { get; set; }
+            public double Price { get; set; }
+            public string[] Purchases { get; set; }
+        }
+
+        public class Distributor
+        {
+            public string Name { get; set; }
+            public string State { get; set; }
+        }
+
+        public class CustDist
+        {
+            public string custName { get; set; }
+            public string distName { get; set; }
+        }
+
+        public class CustDistGroup
+        {
+            public string custName { get; set; }
+            public IEnumerable<string> distName { get; set; }
+        }
+        #endregion
+
+        #region Create data sources
+
+        static List<Customer> customers = new List<Customer>
+        {
+            new Customer {First = "Cailin", Last = "Alford", State = "GA", Price = 930.00, Purchases = new string[] {"Panel 625", "Panel 200"}},
+            new Customer {First = "Theodore", Last = "Brock", State = "AR", Price = 2100.00, Purchases = new string[] {"12V Li"}},
+            new Customer {First = "Jerry", Last = "Gill", State = "MI", Price = 585.80, Purchases = new string[] {"Bulb 23W", "Panel 625"}},
+            new Customer {First = "Owens", Last = "Howell", State = "GA", Price = 512.00, Purchases = new string[] {"Panel 200", "Panel 180"}},
+            new Customer {First = "Adena", Last = "Jenkins", State = "OR", Price = 2267.80, Purchases = new string[] {"Bulb 23W", "12V Li", "Panel 180"}},
+            new Customer {First = "Medge", Last = "Ratliff", State = "GA", Price = 1034.00, Purchases = new string[] {"Panel 625"}},
+            new Customer {First = "Sydney", Last = "Bartlett", State = "OR", Price = 2105.00, Purchases = new string[] {"12V Li", "AA NiMH"}},
+            new Customer {First = "Malik", Last = "Faulkner", State = "MI", Price = 167.80, Purchases = new string[] {"Bulb 23W", "Panel 180"}},
+            new Customer {First = "Serena", Last = "Malone", State = "GA", Price = 512.00, Purchases = new string[] {"Panel 180", "Panel 200"}},
+            new Customer {First = "Hadley", Last = "Sosa", State = "OR", Price = 590.20, Purchases = new string[] {"Panel 625", "Bulb 23W", "Bulb 9W"}},
+            new Customer {First = "Nash", Last = "Vasquez", State = "OR", Price = 10.20, Purchases = new string[] {"Bulb 23W", "Bulb 9W"}},
+            new Customer {First = "Joshua", Last = "Delaney", State = "WA", Price = 350.00, Purchases = new string[] {"Panel 200"}}
+        };
+
+        static List<Distributor> distributors = new List<Distributor>
+        {
+            new Distributor {Name = "Edgepulse", State = "UT"},
+            new Distributor {Name = "Jabbersphere", State = "GA"},
+            new Distributor {Name = "Quaxo", State = "FL"},
+            new Distributor {Name = "Yakijo", State = "OR"},
+            new Distributor {Name = "Scaboo", State = "GA"},
+            new Distributor {Name = "Innojam", State = "WA"},
+            new Distributor {Name = "Edgetag", State = "WA"},
+            new Distributor {Name = "Leexo", State = "HI"},
+            new Distributor {Name = "Abata", State = "OR"},
+            new Distributor {Name = "Vidoo", State = "TX"}
+        };
+
+        static double[] exchange = { 0.89, 0.65, 120.29 };
+
+        static double[] ExchangedPrices = {827.70, 604.50, 111869.70,
+                                        1869.00, 1,365.00, 252609.00,
+                                        521.36, 380.77, 70465.88,
+                                        455.68, 332.80, 61588.48,
+                                        2018.34, 1474.07, 272793.66,
+                                        920.26, 672.10, 124379.86,
+                                        1873.45, 1368.25, 253210.45,
+                                        149.34, 109.07, 20184.66,
+                                        455.68, 332.80, 61588.48,
+                                        525.28, 383.63, 70995.16,
+                                        9.08, 6.63, 1226.96,
+                                        311.50, 227.50, 42101.50};
+
+        static string[] Purchases = {  "Panel 625", "Panel 200",
+                                    "12V Li",
+                                    "Bulb 23W", "Panel 625",
+                                    "Panel 200", "Panel 180",
+                                    "Bulb 23W", "12V Li", "Panel 180",
+                                    "Panel 625",
+                                    "12V Li", "AA NiMH",
+                                    "Bulb 23W", "Panel 180",
+                                    "Panel 180", "Panel 200",
+                                    "Panel 625", "Bulb 23W", "Bulb 9W",
+                                    "Bulb 23W", "Bulb 9W",
+                                    "Panel 200"
+                                 };
+        #endregion
+
+        static void Main(string[] args)
+        {
+            var priceLinq =
+                from x in ExchangedPrices
+                where x < 1000
+                select x;
+            Console.WriteLine("Exercise 1:");
+            foreach (var price in priceLinq)
+            {
+                Console.WriteLine("{0}", price);
+            }
+
+            Console.WriteLine("\nExercise 2a:");
+            var stateQuery =
+                from c in customers  
+                select c;
+            foreach (var c in stateQuery)
+            {
+                Console.WriteLine("{0}", c.First);
+            }
+
+            Console.WriteLine("\nExercise 2b:");
+            var cusFullname =
+                from c in customers
+                select new {FullName = c.First + " " + c.Last};
+
+            foreach (var c in cusFullname)
+            {
+                Console.WriteLine("{0}", c.FullName);
+            }
+
+            Console.WriteLine("\nExercise 2c:");
+            var stateAbb =
+                (from c in customers
+                from d in distributors
+                where c.State.Equals(d.State) 
+                select new {StateName = c.State}).Distinct();
+
+            foreach (var c in stateAbb)
+            {
+                Console.WriteLine("{0}", c.StateName);
+            }
+
+            Console.WriteLine("\nExercise 3a:");
+            var cusTake3 =
+            (from c in customers
+                select c).Take(3);
+
+            foreach (var c in cusTake3)
+            {
+                Console.WriteLine("{0} {1}", c.First, c.Last);
+            }
+
+            Console.WriteLine("\nExercise 3b:");
+            var cusTake3OR =
+            (from c in customers
+             where c.State.Equals("OR")
+             select c).Take(3);
+            
+            foreach (var c in cusTake3OR)
+            {
+                Console.WriteLine("{0} {1} - {2}", c.First, c.Last, c.State);
+            }
+
+            Console.WriteLine("\nExercise 4a:");
+            var cusOrderByFirstname =
+                from c in customers
+                orderby c.First
+                select c;
+            
+            foreach (var c in cusOrderByFirstname)
+            {
+                Console.WriteLine("{0}", c.First);
+            }
+
+            Console.WriteLine("\nExercise 4b:");
+            var cusOrderBylastname =
+                from c in customers
+                orderby c.Last.Length
+                select c;
+
+            foreach (var c in cusOrderBylastname)
+            {
+                Console.WriteLine("{0}", c.Last);
+            }
+
+            Console.WriteLine("\nExercise 4c:");
+            var cusOrderByPrice =
+                from c in customers
+                orderby c.Price descending 
+                select c;
+
+            foreach (var c in cusOrderByPrice)
+            {
+                Console.WriteLine("{0}", c.Price);
+            }
+
+            Console.WriteLine("\nExercise 4d:");
+            var cusOrderByFirstThen =
+                from c in customers
+                orderby c.First.Length, c.Last
+                select c;
+
+            foreach (var c in cusOrderByFirstThen)
+            {
+                Console.WriteLine("{0} {1}", c.First, c.Last);
+            }
+
+            Console.WriteLine("\nExercise 5a:");
+            var cusGroupFirst =
+                from c in customers
+                orderby c.First 
+                group c by c.First[0] into letter
+                select new { FirstLetter = letter.Key, Name = letter };
+
+            foreach (var letter in cusGroupFirst)
+            {
+                Console.WriteLine("{0}:", letter.FirstLetter);
+                foreach (var n in letter.Name)
+                {
+                    Console.WriteLine(n.First);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nExercise 5b:");
+            var cusGroupState =
+                from c in customers
+                orderby c.State
+                group c by c.State into s
+                select new { FirstLetter = s.Key, cust = s };
+
+            foreach (var letter in cusGroupState)
+            {
+                Console.WriteLine("{0}:", letter.FirstLetter);
+                foreach (var n in letter.cust)
+                {
+                    Console.WriteLine(n.First);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nExercise 6:");
+            var custFirst =
+                from c in customers
+                select c.First[0];
+            var custLast =
+                from c in customers
+                select c.Last[0];
+
+            var custNotEqualFirst = custFirst.Except(custLast);
+
+            foreach (var output in custNotEqualFirst)
+            {
+                Console.WriteLine(output);
+            }
+
+            Console.WriteLine("\nExercise 7:");
+
+            bool accepted = customers.Any(c => c.First.Contains("ed"));
+            if(accepted)
+                Console.WriteLine("Yes list contains ed");
+            else
+                Console.WriteLine("no list dosn't contains ed");
+            
+            
+
+            Console.ReadKey();
+        }
+    }
+}
